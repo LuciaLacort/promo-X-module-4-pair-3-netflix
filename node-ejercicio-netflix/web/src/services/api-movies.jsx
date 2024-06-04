17d795b762226e4812518701507dde0aa331b0e2
FatimaDuret <f.duret.g@gmail.com>
2024-06-04 11:00:32 +0200

    endpoints

--- node-ejercicio-netflix/web/src/services/api-movies.jsx
+++ node-ejercicio-netflix/web/src/services/api-movies.jsx
@@ -3,9 +3,17 @@
 const getMoviesFromApi = () => {
   console.log('Se están pidiendo las películas de la app');
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch('//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/empty.json')
+  return fetch('http://localhost:4000/movies'), {
+    
+  method: 'GET',
+ 
+}
     .then(response => response.json())
-    .then(() => {
+    .then(data => {
+      return data;
+    })
+
+   /* .then(() => {
       // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
       return {
         success: true,
@@ -25,8 +33,8 @@ const getMoviesFromApi = () => {
               '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/friends.jpg'
           }
         ]
-      };
-    });
+      }; 
+    }); */
 };
 
 const objToExport = {
